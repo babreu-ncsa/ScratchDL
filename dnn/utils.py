26b1be6d2ec8746449f1b600eae14ac21ab01f29
Bruno Abreu <babreu@illinois.edu>
2023-01-03 14:58:35 -0600

    add gradient_descendent

--- dnn/utils.py
+++ dnn/utils.py
@@ -58,3 +58,50 @@ def one_hot(y):
     one_hot[np.arange(y.shape[0]), y] = 1
 
     return one_hot.T
+
+
+def update_parameters(params, grads, alpha):
+    """
+    Updates the parameters of the network using gradient descent.
+
+    Arguments:
+        - params: dictionary containing the parameters W and b for each layer
+        - grads: dictionary containing the gradients dW and db for each layer
+        - alpha: learning rate
+
+    Returns:
+        - params_updated: dictionary containing the updated parameters W and b for each layer
+    """
+
+    # number of layers
+    L = len(params) // 2
+
+    params_updated = {}
+    for l in range(1, L+1):
+        params_updated['W' + str(l)] = params['W' + str(l)] - alpha * grads['dW' + str(l)]
+        params_updated['b' + str(l)] = params['b' + str(l)] - alpha * grads['db' + str(l)]
+
+    return params_updated
+
+
+def cross_entropy(Y_one_hot, Y_hat, epsilon=1e-12):
+    """
+    Computes cross entropy between the target Y_one_hot (encoded) and the output Y_hat (predicted).
+
+    Arguments:
+        - Y_one_hot: one-hot matrix of the target
+        - Y_hat: predicted output
+        - epsilon: small value to avoid division by zero
+
+    Returns:
+        - crent: cross entropy
+    """
+
+    # clip to avoid division by zero/one
+    Y_hat = np.clip(Y_hat, epsilon, 1. - epsilon)
+
+    # compute cross entropy
+    crent = np.sum(Y_one_hot * np.log(Y_hat), axis=0)
+    crent = -np.mean(crent)
+
+    return crent
