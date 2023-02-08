9c65d8388c04145a070e2ff257674e8b8b609f04
Bruno Abreu <babreu@illinois.edu>
2023-02-08 14:51:16 -0600

    class based code

--- dnn/data_loader.py
+++ dnn/data_loader.py
@@ -1,11 +1,11 @@
 ###
-# File: predictions.py
+# File: data_loaders.py
 # Description: 
 # Author: Bruno R. de Abreu  |  babreu at illinois dot edu
 # National Center for Supercomputing Applications (NCSA)
 #  
-# Creation Date: Tuesday, 3rd January 2023, 2:35:16 pm
-# Last Modified: Tuesday, 3rd January 2023, 2:35:27 pm
+# Creation Date: Tuesday, 3rd January 2023, 3:00:37 pm
+# Last Modified: Tuesday, 3rd January 2023, 3:00:40 pm
 #  
 # Copyright (c) 2023, Bruno R. de Abreu, National Center for Supercomputing Applications.
 # All rights reserved.
@@ -23,16 +23,30 @@
 #          the software and its usage.
 ###
 
-import numpy as np
+import tensorflow as tf
 
-def get_predictions(AL):
-    """
-    Computes the predictions of a neural network.
+class MNISTDataLoader():
+    def __init__(self, one_hot_encode=False):
+        self.one_hot_encode = one_hot_encode
 
-    Arguments:
-        - AL: array of the last activations
+    def load_train_data(self):
+        """
+        Loads the MNIST dataset.
 
-    Returns:
-        - index: the index of the highest activation
-    """
-    return np.argmax(AL, axis=0)
+        Returns:
+            - X_train: training set
+            - Y_train: training labels
+            - X_test: test set
+            - Y_test: test labels
+        """
+        # get it as it comes from keras
+        (X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.mnist.load_data()
+
+        # preprocess
+        x_train = X_train.reshape(-1, 784).astype("float32") / 255.
+        x_test = X_test.reshape(-1,784).astype("float32") / 255.
+        y_train = Y_train.astype("int")
+        y_test = Y_test.astype("int")
+
+
+        return x_train.T, y_train.T
